a0235f73cf28cbb25b5f6c8776515365a492c20e
wangze <w18829898295@163.com>
2018-12-06 09:51:26 +0800

    温度相互转换的代码更改.

---
+++ temperature_up.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+	const unsigned short ADD_SUBTRACT = 32;
+	const double RATIO = 9.0 / 5.0;
+	double temp_in,temp_out;
+	char type_in,type_out;
+
+	std::cout << "请您输入一个温度(输入格式为 xx.x C 或者 xx.x F):";
+	std::cin >> temp_in >> type_in;
+	std::cin.ignore(100,'\n');									//清除键盘缓冲区
+
+	if(type_in == 'C' || type_in == 'c')
+	{
+		temp_out = temp_in * RATIO + ADD_SUBTRACT;
+		type_in = 'C';
+		type_out = 'F';
+	}
+	else if(type_in == 'F' || type_in == 'f')
+	{
+		temp_out = (temp_in - ADD_SUBTRACT) / RATIO;
+		type_in = 'F';
+		type_out = 'C';
+	}
+	else
+	{
+		std::cout << "你输入的不正确!" << std::endl;
+		return 0;
+	}
+	std::cout << "温度转换为:" << temp_in << type_in << " = " << temp_out << type_out << std::endl;
+	//std::cout << "输入任何字符结束!" << '\n';
+	//std::cin.get();
+	return 0;
+}
