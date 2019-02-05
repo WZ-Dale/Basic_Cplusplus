8fda3914a17c22ba83f8d33ddae6cc7feaeee435
wangze <w18829898295@163.com>
2019-02-05 11:02:21 +0800

    新建C++代码,传址异或交换swap1.cpp

---
+++ swap1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void swap(int *a, int *b){
+  *a ^= *b;
+  *b ^= *a;
+  *a ^= *b;
+}
+
+int main(){
+  int a = 0, b = 0;
+  std::cout << "请输入两个不同的整数:";
+  std::cin >> a >> b;
+  std::cout << "交换前:" << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "b = " << b << std::endl;
+  swap(&a,&b);
+  std::cout << "交换后:" << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "b = " << b << std::endl;
+  
+  return 0;
+}
