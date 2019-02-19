fb933eae37f5fbf439d030bd6b6e75bde241a773
wangze <w18829898295@163.com>
2019-02-19 15:19:24 +0800

    覆盖方法

---
+++ Animal_Pig_Turtle3.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+
+
+class Animal{
+  public:
+    std::string mouth;
+    std::string name;
+
+    Animal(std::string theName);
+    void eat();
+    void sleep();
+    void drool();
+};
+
+class Pig : public Animal{
+  public:
+    Pig(std::string theName);
+
+    void climb();
+    void eat();             //new!!
+};
+
+class Turtle : public Animal{
+  public:
+    Turtle(std::string theName);
+
+    void swim();
+    void eat();             //new!!
+};
+
+
+Animal::Animal(std::string theName){
+  name = theName;
+}
+void Animal::eat(){
+  std::cout << "我在吃饭,要不要给你分点..." << std::endl;
+}
+void Animal::sleep(){
+  std::cout << "我在睡觉,请安静哦..." << std::endl;
+}
+void Animal::drool(){
+  std::cout << "我在睡得很香,流口水了..." << std::endl;
+}
+
+
+Pig::Pig(std::string theName) : Animal(theName){
+  //name = theName;
+}
+void Pig::climb(){
+  std::cout << "我是一只母猪,能够爬树的小母猪..." << std::endl;
+}
+void Pig::eat(){
+  Animal::eat();
+  std::cout << "我在吃鱼..." << std::endl;
+}
+
+
+Turtle::Turtle(std::string theName) : Animal(theName){
+  //name = theName;
+}
+void Turtle::swim(){
+  std::cout << "我是一只乌龟,会游泳的小乌龟..." << std::endl;
+}
+void Turtle::eat(){
+  Animal::eat();
+  std::cout << "我在吃东坡肉..." << std::endl;
+}
+
+
+int main(){
+  Pig pig("佩奇");
+  Turtle turtle("爱德华");
+
+  std::cout << "小猪的名字是: " << pig.name << std::endl;
+  std::cout << "小乌龟的名字是: " << turtle.name << std::endl;
+  pig.eat();
+  turtle.eat();
+  pig.climb();
+  turtle.swim();
+
+  return 0;
+};
