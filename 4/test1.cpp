c4d51751b0719b8d86b4de525311ce9860b77891
wangze <w18829898295@163.com>
2019-11-18 22:35:23 +0800

    类型提升

---
+++ 4/test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i = 45;
+    double j = 45.55;
+    cout << i + j << endl;
+    int k = i + j;
+    cout << k << endl;
+
+    return 0;
+}
