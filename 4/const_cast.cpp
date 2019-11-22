4b81492a524fb2b7d7c91a839f128943752fec74
wangze <w18829898295@163.com>
2019-11-22 21:25:35 +0800

    去掉底层const属性

--- 4/const_cast.cpp
+++ 4/const_cast.cpp
@@ -5,12 +5,21 @@ using namespace std;
 int main(){
     char c[] = "abc";
 
-    char *p = &c;
-    const char *cp = &c;
+    char (*p)[4] = &c;
+    const char (*cp)[4] = &c;
 
     cout << c << endl;
     cout << *p << endl;
     cout << *cp << endl;
+
+    char ch = 'a';
+    const char *pc = &ch;
+    char *pp = const_cast<char*>(pc);
+    cout << ch << endl;
+    cout << *pc << endl;
+    cout << *pp << endl;
+
+
 
     return 0;
 }
